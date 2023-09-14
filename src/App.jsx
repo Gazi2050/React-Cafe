39079a6321a7a337ee6c867703f508079f3e31cd
Gazi2050 <gazi45344@gmail.com>
2023-09-14 19:31:46 +0600

    commit NO2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/BookMarks/Bookmarks'
 import Header from './components/header/header'
 function App() {
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
+
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
-
+import profile from '../../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-            <h1 className='text-4xl font-bold text-sky-500'>React Cafe</h1>
-
-        </div>
+        <header className='flex justify-between items-center p-4 mx-4 border-b-2'>
+            <h1 className='text-4xl font-bold text-sky-500'>React <span className='text-amber-500'>Cafe</span></h1>
+            <img src={profile} alt="" />
+        </header>
     );
 };
 
