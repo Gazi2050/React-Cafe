ac28a172e85ec9813dcca0c3ab1386d99bcf79e4
Gazi2050 <gazi45344@gmail.com>
2023-09-14 17:25:21 +0600

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-sky-500'>React Cafe</h1>
+
+        </div>
+    );
+};
+
+export default Header;
